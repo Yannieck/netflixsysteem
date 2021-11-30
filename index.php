1378efd5e4b1249902f59d478cef8b23698aa40d
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2021-11-30 15:54:43 +0100

    Revert "Account inlogscherm toegevoegd"
    This reverts commit b92b8e24df7e939218036d7d5a83ae0bc78d69eb.

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-header('Location: ./pages/landingpage.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <a href="./pages/login.php">Log in</a><br>
+    <a href="./pages/createaccount.php">Create account</a>
+</body>
+</html>
