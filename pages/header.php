9fc8668e1847b7aa284cf24ab49be72d90204677
Leroy Kats <leroy.kats@student.nhlstenden.com>
2021-11-30 21:55:38 +0100

    CSS toegevoegd
    De menubalk wat mooier opgemaakt

--- pages/header.php
+++ pages/header.php
@@ -5,15 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/styles/header.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     <header>
-        <a href="index.php"><img src="../assets/img/lightlogo.svg" height="100" width="100" alt="logo"></a>
-        <ul>
-            <li><a href="">Search</a></li>
-            <li><a href="">Notifications</a></li>
-            <li><a href="">Profile</a></li>
+        <a href="index.php"><img src="../assets/img/lightlogo.svg" height="120" width="120" alt="logo"></a>
+        <ul class="navtekst">
+            <li><a href="">Ask a question</a></li>
+            <li><a href="">Questions</a></li>
+            <li><a href="">Videos</a></li>
+        </ul>
+        <ul class="navicons">
+            <li><a href=""><i class="fas fa-search fa-2x"></i></a></li>
+            <li><a href=""><i class="fas fa-bell fa-2x"></i></a></li>
+            <li><a href=""><i class="fas fa-user fa-2x"></i></a></li>
         </ul>
     </header>
 </body>
