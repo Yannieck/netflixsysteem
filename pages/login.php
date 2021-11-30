6e3e678f87d278aff880b4d46b6c41fefa0c5b80
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2021-11-30 10:48:29 +0100

    Logos toegevoegd en css toegevoegd

--- pages/login.php
+++ pages/login.php
@@ -2,11 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in</title>
+    <link rel="stylesheet" href="../assets/styles/style.css">
 </head>
 <body>
-    
+    <div class="background">
+        <div class="logo">
+            <img src="../assets/img/lightlogo.svg" alt="logo">
+        </div>
+    </div>
 </body>
 </html>
