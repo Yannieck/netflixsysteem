bb70e3c17ab0e987d5d116d8109fb57f5f11bb01
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2021-12-06 15:55:57 +0100

    Aside toegevoegd

--- pages/askquestion.php
+++ pages/askquestion.php
@@ -5,10 +5,14 @@
     <?php include_once('../assets/components/head.php') ?>
     <link rel="stylesheet" href="../assets/styles/login/login.css">
     <link rel="stylesheet" href="../assets/styles/header/header.css">
+    <link rel="stylesheet" href="../assets/styles/aside/aside.css">
 </head>
 
 <body>
-    <?php include_once("../assets/components/header.php") ?>
+    <?php
+    include_once("../assets/components/header.php") ;
+    include_once("../assets/components/aside.php");
+    ?>
 </body>
 
 </html>
