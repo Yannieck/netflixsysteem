b86d5a3f14bbea1e6bf2f936e6b974abb27dab8c
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2021-12-06 14:20:13 +0100

    Header gemerged in main

---
+++ pages/askquestion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once('../assets/components/head.php') ?>
+    <link rel="stylesheet" href="../assets/styles/login/login.css">
+    <link rel="stylesheet" href="../assets/styles/header/header.css">
+</head>
+
+<body>
+    <?php include_once("../assets/components/header.php") ?>
+</body>
+
+</html>
