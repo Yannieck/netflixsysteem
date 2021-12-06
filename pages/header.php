d16c41a7a611cdb9090394c5eb2fc9f12bd55fde
Leroy Kats <leroy.kats@student.nhlstenden.com>
2021-12-06 15:08:07 +0100

    CSS geoptimaliseerd
    CSS fixed

--- pages/header.php
+++ pages/header.php
@@ -10,17 +10,32 @@
 </head>
 <body>
     <header>
-        <a href="index.php"><img src="../assets/img/lightlogo.svg" height="120" width="120" alt="logo"></a>
-        <ul class="navtekst">
-            <li><a href="">Ask a question</a></li>
-            <li><a href="">Questions</a></li>
-            <li><a href="">Videos</a></li>
-        </ul>
-        <ul class="navicons">
-            <li><a href=""><i class="fas fa-search fa-2x"></i></a></li>
-            <li><a href=""><i class="fas fa-bell fa-2x"></i></a></li>
-            <li><a href=""><i class="fas fa-user fa-2x"></i></a></li>
-        </ul>
+        <div class="leftHeader">
+            <a href="index.php"><img src="../assets/img/lightlogo.svg" height="120" width="120" alt="logo"></a>
+            <ul class="navtekst">
+                <li><a href="">Ask a question</a></li>
+                <li><a href="">Questions</a></li>
+                <li><a href="">Videos</a></li>
+            </ul>
+        </div>
+        <div class="rightHeader">
+            <ul class="navicons">
+                <li><input id="zoekBalk" type="text"></li>
+                <li><i onclick="showInput()" class="fas fa-search fa-2x"></i></li>
+                <li><a href=""><i class="fas fa-bell fa-2x"></i></a></li>
+                <li><a href=""><i class="fas fa-user fa-2x"></i></a></li>
+            </ul>
+        </div>
     </header>
+    <script>
+        const zoekbalkIcoon = document.getElementById("zoekBalk");
+        function showInput(){
+            if(zoekbalkIcoon.style.display == "inline"){
+                zoekbalkIcoon.style.display = "none";
+            }else{
+                zoekbalkIcoon.style.display = "inline";
+            }
+        }
+    </script>
 </body>
 </html>
