37af584f8efdb4967dcb25e0e51872fbf784db8c
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2021-12-06 19:00:31 +0100

    Geupdate header gemerged

--- assets/components/head.php
+++ assets/components/head.php
@@ -1,3 +1,4 @@
 <meta charset="UTF-8">
 <title>Null Pointer Videos</title>
 <link rel="stylesheet" href="../assets/styles/style.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
