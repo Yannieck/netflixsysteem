8cd08687fc012706ba3a34b738628eee60b80ea6
Leroy Kats <leroy.kats@student.nhlstenden.com>
2021-12-07 12:57:48 +0100

    begin van profiel pagina
    begin gemaakt

---
+++ pages/profile.php
@@ -0,0 +1,50 @@
+<head>
+    <?php include_once('../assets/components/head.php') ?>
+    <link rel="stylesheet" href="../assets/styles/header/header.css">
+    <link rel="stylesheet" href="../assets/styles/profile/profile.css">
+</head>
+<body>
+<?php include_once('../assets/components/header.php') ?>
+    <div class="container">
+        <div class="contentBlock">
+            <div class="profileBlock">
+                <h1>Profile</h1>
+                <form action="">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" placeholder="Name">
+
+                </form>
+            </div>
+            <div class="membershipBlock">
+                <h1>Select membership</h1>
+                <table>
+                    <tr>
+                        <td>Monthly price</td>
+                        <td>&#8364;9,99</td>
+                    </tr>
+                    <tr>
+                        <td>HD available</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>4K available</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Watch on your laptop or TV</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Unlimited videos</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Cancel anytime</td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    
+</body>
