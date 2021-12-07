d558760b662a695d8f06fa95c230faecb418cbe0
Francoiskral <francois.kral@student.nhlstenden.com>
2021-12-07 14:46:17 +0100

    main opzetje

---
+++ pages/main.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once('../assets/components/head.php') ?>
+    <link rel="stylesheet" href="../assets/styles/header/header.css">
+    <link rel="stylesheet" href="../assets/styles/style.css">
+    <link rel="stylesheet" href="../assets/styles/aside/aside.css">
+    <link rel="stylesheet" href="../assets/styles/mainpage/main.css">
+</head>
+
+<body>
+    <?php
+    include_once("../assets/components/header.php") ; 
+    ?>
+    <div class="flex">
+        <?php  include_once("../assets/components/aside.php"); ?>
+        <div class="main-container">
+
+        </div>
+    </div>
+</body>
+
+</html>
