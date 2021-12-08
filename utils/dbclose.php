f28ffb89c0988110236460f46ee639bb39e23d47
ItzJoris03 <jorishummel03@gmail.com>
2021-12-08 14:14:12 +0100

    Database connection files

---
+++ utils/dbclose.php
@@ -0,0 +1,15 @@
+<?php
+
+///////////////////////////////////////////////////////
+//                                                   //
+// File:        dbclose.php                          //
+// Created:     08-12-2021                           //
+// By:          Joris Hummel                         //
+// Description: Close connection with the database.  //
+//                                                   //
+///////////////////////////////////////////////////////
+
+mysqli_close($conn);
+
+
+?>
---
+++ utils/dbconnect.php
@@ -0,0 +1,22 @@
+<?php
+
+//////////////////////////////////////////////
+//                                          //
+// File:        dbconnect.php               //
+// Created:     08-12-2021                  //
+// By:          Joris Hummel                //
+// Description: Connect with the database.  //
+//                                          //
+//////////////////////////////////////////////
+
+$host = "localhost";
+$user = "root";
+$pwd = "";
+$db = "null pointer videos";
+
+$conn = mysqli_connect($host, $user, $pwd, $db) 
+    OR DIE('Cannot connect to the database.');
+
+echo "Connection successfull";
+
+?>
