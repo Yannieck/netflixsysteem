73102fa0da03a16b09dcceb54f8f5cf064e0ac6f
Francoiskral <francois.kral@student.nhlstenden.com>
2021-12-15 15:49:01 +0100

    Main.php afgemaakt

--- pages/main.php
+++ pages/main.php
@@ -1,4 +1,3 @@
-<?php include_once("../assets/components/loginCheck.php") ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,46 +11,82 @@
 
 <body>
     <?php
-    include_once("../assets/components/header.php");
+    include_once("../assets/components/header.php") ; 
     ?>
     <div class="flex">
-        <?php include_once("../assets/components/aside.php"); ?>
+        <?php  include_once("../assets/components/aside.php"); ?>
         <div class="pageContent">
             <div class="filter">
                 <div class="flex-container">
-                    <div class="logos">
+                    <div class="logos" onclick="redirect('#')">
                         <img src="../assets/icons/HTML.svg" alt="HTML">
                         <p>&lt;HTML&sol;&gt;</p>
                     </div>
-                    <div class="logos">
+                    <div class="logos" onclick="redirect('#')">
                         <img src="../assets/icons/PHP.svg" alt="php">
                         <p>&lt;?php</p>
                     </div>
-                    <div class="logos">
+                    <div class="logos" onclick="redirect('#')">
                         <img src="../assets/icons/JavaScript.svg" alt="JavaScript">
                         <p>Javascript;</p>
                     </div>
-                    <div class="logos">
+                    <div class="logos" onclick="redirect('#')">
                         <img src="../assets/icons/CSharp.svg" alt="C#">
                         <p>C#</p>
                     </div>
-                    <div class="logos">
+                    <div class="logos" onclick="redirect('#')">
                         <img src="../assets/icons/CSS.svg" alt="CSS">
                         <p>CSS</p>
                     </div>
-                    <div class="logos">
+                    <div class="logos" onclick="redirect('#')">
                         <img src="../assets/icons/Java.svg" alt="Java">
                         <p>Java</p>
                     </div>
-                    <div class="logos">
+                    <div class="logos" onclick="redirect('#')">
                         <img src="../assets/icons/Python.svg" alt="Python">
                         <p>Python</p>
                     </div>
-                    <div class="logos">
+                    <div class="logos" onclick="redirect('#')">
                         <img src="../assets/icons/Android.svg" alt="Android">
                         <p>Android</p>
                     </div>
-                    <div class="logos">
+                    <div class="logos" onclick="redirect('#')">
+                        <img src="../assets/icons/Apple.svg" alt="Apple">
+                        <p>Apple</p>
+                    </div>                    
+                    <div class="logos" onclick="redirect('#')">
+                        <img src="../assets/icons/HTML.svg" alt="HTML">
+                        <p>&lt;HTML&sol;&gt;</p>
+                    </div>
+                    <div class="logos" onclick="redirect('#')">
+                        <img src="../assets/icons/PHP.svg" alt="php">
+                        <p>&lt;?php</p>
+                    </div>
+                    <div class="logos" onclick="redirect('#')">
+                        <img src="../assets/icons/JavaScript.svg" alt="JavaScript">
+                        <p>Javascript;</p>
+                    </div>
+                    <div class="logos" onclick="redirect('#')">
+                        <img src="../assets/icons/CSharp.svg" alt="C#">
+                        <p>C#</p>
+                    </div>
+                    <div class="logos" onclick="redirect('#')">
+                        <img src="../assets/icons/CSS.svg" alt="CSS">
+                        <p>CSS</p>
+                    </div>
+                    <div class="logos" onclick="redirect('#')">
+                        <img src="../assets/icons/Java.svg" alt="Java">
+                        <p>Java</p>
+                    </div>
+                    <div class="logos" onclick="redirect('#')">
+                        <img src="../assets/icons/Python.svg" alt="Python">
+                        <p>Python</p>
+                    </div>
+                    <div class="logos" onclick="redirect('#')">
+                        <img src="../assets/icons/Android.svg" alt="Android">
+                        <p>Android</p>
+                    </div>
+                    <div class="logos" onclick="redirect('#')">
                         <img src="../assets/icons/Apple.svg" alt="Apple">
                         <p>Apple</p>
                     </div>
@@ -59,6 +94,11 @@
             </div>
         </div>
     </div>
-</body>
+    <script>
+        function redirect(location){
+            window.location.href=location;
+        }
 
+    </script>
+</body>
 </html>
