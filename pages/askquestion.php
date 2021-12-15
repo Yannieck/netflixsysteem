10cd2d9ba5d3df42603f2896d3e941aa673fadca
BartHerms <91067528+BartHerms@users.noreply.github.com>
2021-12-15 13:55:39 +0100

    Update askquestion.php

--- pages/askquestion.php
+++ pages/askquestion.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php include_once('../assets/components/head.php') ?>
-    <?php include_once("../assets/components/header.php"); ?>
+     
     <link rel="stylesheet" href="../assets/styles/login/login.css">
     <link rel="stylesheet" href="../assets/styles/header/header.css">
     <link rel="stylesheet" href="../assets/styles/aside/aside.css">
@@ -11,8 +11,9 @@
 </head>
 
 <body>
-    
+<?php include_once("../assets/components/header.php"); ?>
     <div class="flex">
+          
         <?php include_once("../assets/components/aside.php"); ?>
         <div class="pageContent">
             <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
