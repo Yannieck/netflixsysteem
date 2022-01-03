831387471b7ef607cf05688e2cb31b574f694874
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2022-01-03 14:58:38 +0100

    Askquestions database geinclude

--- pages/askquestion.php
+++ pages/askquestion.php
@@ -1,9 +1,12 @@
-<?php include_once("../assets/components/loginCheck.php") ?>
+<?php
+include_once("../assets/components/loginCheck.php");
+include_once("../utils/dbconnect.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <?php include_once('../assets/components/head.php') ?>     
+    <?php include_once('../assets/components/head.php') ?>
     <link rel="stylesheet" href="../assets/styles/login/login.css">
     <link rel="stylesheet" href="../assets/styles/header/header.css">
     <link rel="stylesheet" href="../assets/styles/aside/aside.css">
@@ -12,7 +15,7 @@
 
 <body>
     <?php include_once("../assets/components/header.php"); ?>
-    <div class="flex">      
+    <div class="flex">
         <?php include_once("../assets/components/aside.php"); ?>
         <div class="pageContent">
             <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
@@ -20,40 +23,42 @@
                 <input type="text" name="title" id="title" placeholder="Title...">
                 <label for="text">Enter your question here:</label>
                 <textarea name="text" id="text" placeholder="Question..."></textarea>
-                <input name="submit"class="button" type="submit" value="Submit">
+                <input name="submit" class="button" type="submit" value="Submit">
             </form>
             <!-- <p id="output"> -->
-                <?php
-                // echo htmlentities($_POST['title']);
-                // echo "<br>";
-                // $rawinput = $_POST['text'];
-                // if (str_contains($rawinput, "<<") && str_contains($rawinput, ">>")) {
-                //     $begin = strpos($rawinput, "<<") + 1;
-                //     $end = strpos($rawinput, ">>") - 1;
-                //     $len = $end - $begin;
-                //     $code = substr($rawinput, $begin + 1, $len);
-                //     $otherText = explode($code, $rawinput);
-                //     echo htmlentities(substr($otherText[0], 0, -2));
-                //     echo "<code>";
-                //     echo $code;
-                //     echo "</code>";
-                //     echo htmlentities(substr($otherText[1], 2, strlen($otherText[1])));
-                // } else {
-                //     echo htmlentities($rawinput);
-                // }
+            <?php
+            // echo htmlentities($_POST['title']);
+            // echo "<br>";
+            // $rawinput = $_POST['text'];
+            // if (str_contains($rawinput, "<<") && str_contains($rawinput, ">>")) {
+            //     $begin = strpos($rawinput, "<<") + 1;
+            //     $end = strpos($rawinput, ">>") - 1;
+            //     $len = $end - $begin;
+            //     $code = substr($rawinput, $begin + 1, $len);
+            //     $otherText = explode($code, $rawinput);
+            //     echo htmlentities(substr($otherText[0], 0, -2));
+            //     echo "<code>";
+            //     echo $code;
+            //     echo "</code>";
+            //     echo htmlentities(substr($otherText[1], 2, strlen($otherText[1])));
+            // } else {
+            //     echo htmlentities($rawinput);
+            // }
 
-                // if (isset($_POST["submit"])) { 
-                //     if (!empty($_POST["title"])) {
-                //         // $titleError = test_input($_POST["title"]);
-                //     } else { 
-                        
-                //         echo '<span style="color:red;"> Title is required! </span>';
-                //     }
-                // }
-                ?>
+            // if (isset($_POST["submit"])) { 
+            //     if (!empty($_POST["title"])) {
+            //         // $titleError = test_input($_POST["title"]);
+            //     } else { 
+
+            //         echo '<span style="color:red;"> Title is required! </span>';
+            //     }
+            // }
+            ?>
             <!-- </p> -->
         </div>
     </div>
 </body>
-
+<?php
+include_once("../utils/dbclose.php");
+?>
 </html>
