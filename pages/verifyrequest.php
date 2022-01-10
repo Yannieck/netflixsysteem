38df9bccfe2e12bc5aeb11ea338a54d884cc224e
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2022-01-10 15:25:40 +0100

    Verify request pagina aangepast

--- pages/verifyrequest.php
+++ pages/verifyrequest.php
@@ -12,13 +12,10 @@
         <a href="./landingpage.php"><img class="logo" src="../assets/img/lightlogo.svg" alt="logo"></a>
         <div class="content width-2">
             <h1>Verified account</h1>
-            <p>This is where professional programmers can submit their information to possibly get a professional account.
-                This account will be free of charge and you will have a checkmark next to your name.</p>
-
+            <p>This is where professional programmers can submit their information to possibly get a professional account. This account will be free of charge and you will have a checkmark next to your name.</p>
             <form action="<?php echo htmlentities($_SERVER['PHP_SELF']) ?>" method="post" enctype="multipart/form-data">
                 <div class="flex">
                     <div class="half-form">
-
                         <!-- Voledige naam -->
                         <input class="formfield" type="text" name="fullname" id="fullname" placeholder="Full name...">
                         <p class="errortext" id="nameerror">Please enter a valid name.</p>
@@ -26,6 +23,12 @@
                         <!-- Email veld -->
                         <input class="formfield" type="email" name="email" id="email" placeholder="Email...">
                         <p class="errortext" id="emailerror">Please enter a valid emailadress.</p>
+                        <p class="errortext" id="comboerror">This combination of username and email is not in our system. Consider creating a account first.</p>
+
+                        <!-- Wachtwoord veld -->
+                        <input class="formfield" type="password" name="password" id="password" placeholder="Password...">
+                        <p class="errortext" id="passworderror">Please enter your password.</p>
+                        <p class="errortext" id="passwordincorrect">This password is incorrect, please try again.</p>
 
                         <!-- Submit -->
                         <input class="formbutton" type="submit" name="login" id="login" value="Sign In">
@@ -69,87 +72,109 @@
     // Form input valideren
     if (isset($_POST['login'])) {
         if (!empty($_POST['fullname'])) {
-            if (filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
-                if (!empty($_FILES["uploadedFile"])) {
-                    // Grootte kleiner dan 60 kb
-                    if ($_FILES["uploadedFile"]["size"] < 60000) {
-                        $acceptedFileTypes = ["image/png", "image/jpg", "image/jpeg"];
-                        $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
-                        $uploadedFileType = finfo_file($fileinfo, $_FILES["uploadedFile"]["tmp_name"]);
+            if (!empty($_POST['password'])) {
+                $fullname = filter_input(INPUT_POST, 'fullname', FILTER_SANITIZE_SPECIAL_CHARS);
+                if (filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
+                    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+                    if (!empty($_FILES["uploadedFile"]["tmp_name"])) {
+                        // Kijk of het account bestaat door naar de gegevens te zoeken in de database.
+                        $sql = "SELECT account.`password` FROM account WHERE account.`name` = ? AND email = ?";
+                        $stmt = mysqli_prepare($conn, $sql);
+                        mysqli_stmt_bind_param($stmt, "ss", $fullname, $email);
+                        mysqli_stmt_execute($stmt) or die(mysqli_error($conn));
+                        mysqli_stmt_bind_result($stmt, $passResult) or die(mysqli_error($conn));
+                        mysqli_stmt_store_result($stmt);
+                        mysqli_stmt_fetch($stmt);
 
-                        // De plek waar de uploads heen gaan
-                        $saveDir = "../assets/upload/verify-request/";
+                        // Kijk of er resultaten zijn.
+                        if (mysqli_stmt_num_rows($stmt) > 0) {
+                            if (password_verify($_POST['password'], $passResult)) {
+                                // Grootte kleiner dan 60 kb
+                                if ($_FILES["uploadedFile"]["size"] < 60000) {
+                                    $acceptedFileTypes = ["image/png", "image/jpg", "image/jpeg"];
+                                    $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
+                                    $uploadedFileType = finfo_file($fileinfo, $_FILES["uploadedFile"]["tmp_name"]);
 
-                        // Check of de bestandsextentie een van de boven genoemde extenties is
-                        if (in_array($uploadedFileType, $acceptedFileTypes)) {
-                            // Bestaat het bestand al?
-                            if (!file_exists($saveDir . $_FILES["uploadedFile"]["name"])) {
-                                // Haal een naam en email uit de form en geef het bestand die naam
-                                $fullname = str_replace(" ", "-", filter_input(INPUT_POST, 'fullname', FILTER_SANITIZE_SPECIAL_CHARS));
-                                $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+                                    // De plek waar de uploads heen gaan
+                                    $saveDir = "../assets/upload/verify-request/";
 
-                                $newFileName = "REQUEST_" . $fullname . "_" . $email;
+                                    // Check of de bestandsextentie een van de boven genoemde extenties is
+                                    if (in_array($uploadedFileType, $acceptedFileTypes)) {
+                                        // Bestaat het bestand al?
+                                        if (!file_exists($saveDir . $_FILES["uploadedFile"]["name"])) {
+                                            // Haal een naam en email uit de form en geef het bestand die naam
+                                            $fullname = str_replace(" ", "-", filter_input(INPUT_POST, 'fullname', FILTER_SANITIZE_SPECIAL_CHARS));
+                                            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 
-                                // Haal de extentie van het bestand op
-                                $ext = explode(".", $_FILES["uploadedFile"]["name"])[1];
-                                echo $ext;
+                                            $newFileName = "REQUEST_" . $fullname . "_" . $email;
 
-                                // Upload het bestand
-                                if (move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $saveDir . $newFileName . "." . $ext)) {
-                                    header("Location: ./verifyredirect.php");
+                                            // Haal de extentie van het bestand op
+                                            $ext = explode(".", $_FILES["uploadedFile"]["name"])[1];
+
+                                            // Upload het bestand
+                                            if (move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $saveDir . $newFileName . "." . $ext)) {
+                                                header("Location: ./verifyredirect.php", true, 301);
+                                                exit();
+                                            } else {
+                                                echo "An error occured, try again later";
+                                            }
+                                        } else {
+                                            echo
+                                            "<script>
+                                                document.getElementById('uploaduploaderror').style.display = 'block';
+                                            </script>";
+                                        }
+                                    } else {
+                                        echo
+                                        "<script>
+                                            document.getElementById('uploadexterror').style.display = 'block';
+                                        </script>";
+                                    }
                                 } else {
-                                    echo "An error occured, try again later";
+                                    echo
+                                    "<script>
+                                        document.getElementById('uploadsizeerror').style.display = 'block';
+                                    </script>";
                                 }
                             } else {
                                 echo
                                 "<script>
-                                    document.getElementById('uploaduploaderror').style.display = 'block';
+                                    document.getElementById('passwordincorrect').style.display = 'block';
                                 </script>";
                             }
                         } else {
+                            // Stukje javascript dat de display van de error van 'none' naar 'block' verandert
                             echo
                             "<script>
-                                document.getElementById('uploadexterror').style.display = 'block';
+                                document.getElementById('comboerror').style.display = 'block';
+                                document.getElementById('email').value = '" . $_POST['email'] . "';
+                                document.getElementById('fullname').value = '" . $_POST['fullname'] . "';    
                             </script>";
                         }
+                        Mysqli_stmt_close($stmt);
                     } else {
+                        // Stukje javascript dat de display van de error van 'none' naar 'block' verandert
                         echo
                         "<script>
-                            document.getElementById('uploadsizeerror').style.display = 'block';
+                            document.getElementById('uploaderror').style.display = 'block';
+                            document.getElementById('email').value = '" . $_POST['email'] . "';
+                            document.getElementById('fullname').value = '" . $_POST['fullname'] . "';    
                         </script>";
                     }
                 } else {
+                    // Stukje javascript dat de display van de error van 'none' naar 'block' verandert
                     echo
                     "<script>
-                        document.getElementById('uploaderror').style.display = 'block';
+                        document.getElementById('emailerror').style.display = 'block';
+                        document.getElementById('email').value = '" . $_POST['email'] . "';
+                        document.getElementById('fullname').value = '" . $_POST['fullname'] . "';                        
                     </script>";
                 }
-
-                // Database connectie
-                // $query = "SELECT `Password` FROM `account` WHERE Email = ?";
-
-                // $stmt = mysqli_prepare($conn, $query);
-                // mysqli_stmt_bind_param($stmt, 's', $email);
-
-                // mysqli_stmt_execute($stmt) or die(mysqli_error($conn));
-
-                // mysqli_stmt_bind_result($stmt, $passResult) or die(mysqli_error($conn));
-                // mysqli_stmt_store_result($stmt);
-
-                // mysqli_stmt_fetch($stmt);
-
-                // if (password_verify($_POST['password'], $passResult)) {
-                //     header("Location: ./loginredirect.php?rem=" . $remember);
-                // } else {
-                //     echo "ERROR: Password does not match emailadress.";
-                // }
-                // mysqli_stmt_close($stmt);
             } else {
                 // Stukje javascript dat de display van de error van 'none' naar 'block' verandert
-                // en het email veld weer invult
                 echo
                 "<script>
-                    document.getElementById('emailerror').style.display = 'block';
+                    document.getElementById('passworderror').style.display = 'block';
                 </script>";
             }
         } else {
