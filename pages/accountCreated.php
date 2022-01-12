552cf292d85acbff6422ffc8cf50de56f8ba68ea
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2022-01-12 10:53:48 +0100

    Accountcreated naamgeving aangepast

--- pages/accountCreated.php
+++ pages/accountCreated.php
@@ -3,14 +3,13 @@
 
 <head>
     <?php include_once('../assets/components/head.php') ?>
-    <link rel="stylesheet" href="../assets/styles/style.css">
     <link rel="stylesheet" href="../assets/styles/login/login.css">
 </head>
 
 <body>
     <div class="background center">
         <a href="./landingpage.php"><img class="logo" src="../assets/img/lightlogo.svg" alt="logo"></a>
-        <div class="page">
+        <div class="content">
             <h1>Account created</h1>
             <p>Your account had been created. You can log in now.</p>
             <a href="./login.php" class="button">Log in</a>
